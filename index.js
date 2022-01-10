b0b41e7aae0449ab0cb3d1f608e14413982164dd
Eveline Guerrero <evelineguerrero23@gmail.com>
2022-01-09 21:04:56 -0500

    Completed

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  let wordArray = word.split('');
+  let reversed = wordArray.reverse().join('');
+ 
+    if(word === reversed){
+      return true;
+    } else {
+    return false;
+    }
+  }
+
+
 
 /* 
   Add your pseudocode here
+  let wordArray be string solit into array
+  reverse array
+  if(word at index is the same as wordReversed at index)
+  return true
+  else 
+  return false 
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
